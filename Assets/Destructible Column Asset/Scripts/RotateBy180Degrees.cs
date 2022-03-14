b55eb006082cce359b7d065e01256d8d37807a59
fahidah <fahidah.abdussalam@gmail.com>
2022-03-14 11:50:45 +0100

    Initial commit

---
+++ Assets/Destructible Column Asset/Scripts/RotateBy180Degrees.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBy180Degrees : MonoBehaviour
+{
+    float rotationSpeed = 1.0f;
+
+    void OnMouseDrag()
+    {
+        float rotateOnXaxis = Input.GetAxis("Mouse X") * rotationSpeed;
+
+        transform.Rotate(Vector3.down, rotateOnXaxis);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# kaalika
+ This is an assessment for an internship posotion at Kaalika-ai. The project is on webgl build, that allows users to rotate a column about 180 degrees and also simulate an explosion system
